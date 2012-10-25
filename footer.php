1036bec261a7760fb38bb6e49e6f331b3f99630d
Santiago Gil <gil.educaciontdf@gmail.com>
2012-10-25 12:39:03 -0300

    Removed copyright notice

--- footer.php
+++ footer.php
@@ -13,7 +13,6 @@
       <footer>
 <div class="container">
       <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; <?php bloginfo('name'); ?> <?php the_time('Y') ?></p>
           <?php
     if ( function_exists('dynamic_sidebar')) dynamic_sidebar("footer-content");
 ?>
